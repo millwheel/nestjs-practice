186ddab86478f67c1a4f08456edc9cbcbfd54c4d
season <sionwer4@naver.com>
2023-09-17 21:21:21 +0900

    feat: Add validation pipe

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -9,6 +9,7 @@ import {
   Query,
 } from '@nestjs/common';
 import { MoviesService } from './movies.service';
+import { CreateMovieDto } from './dto/create-movie.dto';
 
 @Controller('movies')
 export class MoviesController {
@@ -30,7 +31,7 @@ export class MoviesController {
   }
 
   @Post()
-  create(@Body() movieData) {
+  create(@Body() movieData: CreateMovieDto) {
     return this.moviesService.create(movieData);
   }
 
